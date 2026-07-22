232a557550bfe0382449da1e7734f315ad98b0c5
Bassem <apple@apples-MacBook-Air.local>
2026-07-22 23:37:18 +0300

    backend and front end

--- backend/routes/siteSetting.js
+++ backend/routes/siteSetting.js
@@ -9,10 +9,8 @@ const {
 
 const verifyToken = require("../middleware/auth");
 const authorize = require("../middleware/role");
-// أي حد يقدر يقرأ إعدادات الموقع
 router.get("/", getSiteSettings);
 
-// الأدمن فقط يقدر يعدلها
 router.patch("/", verifyToken, authorize("admin"), updateSiteSettings);
 
 module.exports = router;
